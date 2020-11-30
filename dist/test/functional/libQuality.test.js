ff3f38beadd0f03382c78cf7b0f15c6d66dab935
Romulo Ramos <romulo.ramos@gee.inatel.br>
2020-11-30 16:20:38 -0400

    Cleaning up

--- dist/test/functional/libQuality.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-describe('Git parameters for repository searchs', () => {
-    it('Should return the parameters to some repositoty searched', async () => {
-        const { body, status } = await global.testRequest.get('/gitRequest');
-        expect(status).toBe(200);
-        expect(body).toEqual([{ resp: 'TODO - Objeto de retorno' }]);
-    });
-});
-//# sourceMappingURL=libQuality.test.js.map
